010206aee8bb227ac17500b61bcc34277bbaedc9
WebDevs.pro <shock_31337@ukr.net>
2024-11-05 08:42:20 +0200

    1.1.1

--- inc/plugin.php
+++ inc/plugin.php
@@ -13,7 +13,7 @@ function dcf_register_assets() {
 	// Register JS file
 	wp_enqueue_script(
 		'domu-custom-js', // Handle for the script
-		DCF_PLUGIN_DIR_URL  . '/inc/assets/domu-custom.js',
+		DCF_PLUGIN_DIR_URL  . '/inc/shortcodes/assets/domu-custom.js',
 		array( 'jquery' ),
 		DCF_PLUGIN_VERSION,
 		true // Load in the footer
@@ -22,7 +22,7 @@ function dcf_register_assets() {
 	// Register CSS file
 	wp_enqueue_style(
 		'domu-custom-css', // Handle for the style
-		DCF_PLUGIN_DIR_URL  . '/inc/assets/domu-custom.css',
+		DCF_PLUGIN_DIR_URL  . '/inc/shortcodes/assets/domu-custom.css',
 		array(), // Dependencies (empty array means no dependencies)
 		DCF_PLUGIN_VERSION,
 		'all' // Media type: 'all', 'screen', 'print', etc.
