546726bc56c07c0f665cddb1dba8afdcc00804dd
WebDevs.pro <shock_31337@ukr.net>
2024-11-25 03:56:04 +0200

    1.6.2

--- domu-custom-functions.php
+++ domu-custom-functions.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Domu Custom Functions
  * Plugin URI: https://frymo.de
- * Version: 1.6.1
+ * Version: 1.6.2
  * Description: Custom functions for Domu.
  * Author: Stark Systems UG
  * Author URI: https://stark-systems.de
