4afe904b8e4f63debab44c3b9c581cb759ce8ffb
WebDevs.pro <shock_31337@ukr.net>
2024-12-04 07:52:52 +0200

    1.10.2

--- inc/elementor/widgets/services-grid.php
+++ inc/elementor/widgets/services-grid.php
@@ -45,36 +45,50 @@ class DCF_Services_Grid extends Elementor\Widget_Base {
 			'label' => esc_html__( 'Content', 'frymo' ),
 		] );
 
-         $this->add_control( 'fallback_icon', [
-            'label' => 'Fallback Icon', 
-            'type' => \Elementor\Controls_Manager::MEDIA,
-            'default' => [
-               'url' => \Elementor\Utils::get_placeholder_image_src(),
-            ],
-         ] );
+			$this->add_responsive_control( 'columns', [
+				'label'          => esc_html__( 'Anzahl Spalten', 'frymo' ),
+				'type'           => Controls_Manager::NUMBER,
+				'min'            => 1,
+				'max'            => 6,
+				'step'           => 1,
+				'default'        => 2,
+				'tablet_default' => 2,
+				'mobile_default' => 1,
+				'selectors'      => [
+					'{{WRAPPER}} .dcf-services-grid' => 'grid-template-columns: repeat({{VALUE}}, 1fr);',
+				],
+			] );
+
+				$this->add_control( 'fallback_icon', [
+					'label' => 'Fallback Icon', 
+					'type' => \Elementor\Controls_Manager::MEDIA,
+					'default' => [
+						'url' => \Elementor\Utils::get_placeholder_image_src(),
+					],
+				] );
 
 		$this->end_controls_section();
 
-      Frymo_Elementor::get_heading_controls_section( $this, 'h3', esc_html__( 'Ausstattung', 'frymo' ), true, true );
+		Frymo_Elementor::get_heading_controls_section( $this, 'h3', esc_html__( 'Ausstattung', 'frymo' ), true, true );
 
 		$this->start_controls_section( 'section_style', [
 			'label' => esc_html__( 'Style', 'frymo' ),
-         'tab'   => Elementor\Controls_Manager::TAB_STYLE,
+			'tab'   => Elementor\Controls_Manager::TAB_STYLE,
 		] );
 
-         $this->add_control( 'content_color', [
-            'label'     => esc_html__( 'Color', 'elementor' ),
-            'type'      => Controls_Manager::COLOR,
-            'selectors' => [
-               '{{WRAPPER}} .dcf-service-label' => 'color: {{VALUE}};',
-            ],
-         ] );
+			$this->add_control( 'content_color', [
+				'label'     => esc_html__( 'Color', 'elementor' ),
+				'type'      => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .dcf-service-label' => 'color: {{VALUE}};',
+				],
+			] );
 
-         $this->add_group_control( Group_Control_Typography::get_type(), [
-            'name'     => 'content_typography',
-            'label'    => esc_html__( 'Typography', 'elementor' ),
-            'selector' => '{{WRAPPER}} .dcf-service-label',
-         ] );
+			$this->add_group_control( Group_Control_Typography::get_type(), [
+				'name'     => 'content_typography',
+				'label'    => esc_html__( 'Typography', 'elementor' ),
+				'selector' => '{{WRAPPER}} .dcf-service-label',
+			] );
 
 		$this->end_controls_section();
 
@@ -86,40 +100,40 @@ class DCF_Services_Grid extends Elementor\Widget_Base {
 	protected function render() {
 		$settings = $this->get_settings();
 
-      // Fetch all terms attached to the post for the 'udft_ausstattungen' taxonomy
-      $terms = wp_get_post_terms( get_the_ID(), 'udft_ausstattungen' );
+		// Fetch all terms attached to the post for the 'udft_ausstattungen' taxonomy
+		$terms = wp_get_post_terms( get_the_ID(), 'udft_ausstattungen' );
 
-      if ( ( ! is_wp_error( $terms ) && ! empty( $terms ) ) || Elementor\Plugin::$instance->editor->is_edit_mode() ) {
+		if ( ( ! is_wp_error( $terms ) && ! empty( $terms ) ) || Elementor\Plugin::$instance->editor->is_edit_mode() ) {
 			Frymo_Elementor::render_heading( $this );
 		}
 
 
-      if ( is_wp_error( $terms ) || empty( $terms ) ) {
-         return;
-      }
+		if ( is_wp_error( $terms ) || empty( $terms ) ) {
+			return;
+		}
 
-      echo '<ul class="dcf-services-grid">';
+		echo '<ul class="dcf-services-grid">';
 
-      foreach ( $terms as $term ) {
+		foreach ( $terms as $term ) {
 
-         $icon_id = get_term_meta( $term->term_id, 'icon', true ) ?: $settings['fallback_icon']['id'];
+			$icon_id = get_term_meta( $term->term_id, 'icon', true ) ?: $settings['fallback_icon']['id'];
 
-         echo '<li class="dcf-service">';
+			echo '<li class="dcf-service">';
 
-            echo '<span class="dcf-service-icon">';
+				echo '<span class="dcf-service-icon">';
 
-            if ( ! empty( $icon_id ) ) {
-               echo wp_get_attachment_image( $icon_id, 'full', false );
-            }
+				if ( ! empty( $icon_id ) ) {
+					echo wp_get_attachment_image( $icon_id, 'full', false );
+				}
 
-            echo '</span>';
+				echo '</span>';
 
-            echo '<span class="dcf-service-label">' . $term->name . '</span>';
+				echo '<span class="dcf-service-label">' . $term->name . '</span>';
 
-         echo '</li>';
-      }
+			echo '</li>';
+		}
 
-      echo '</ul>';
+		echo '</ul>';
 	}
 
 }
