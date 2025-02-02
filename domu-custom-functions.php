851843628be9840c94b61f077f370798b1caf2a2
Alex Ishchenko <shock_31337@ukr.net>
2025-02-02 14:33:27 +0200

    1.17.2

--- domu-custom-functions.php
+++ domu-custom-functions.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Domu Custom Functions
  * Plugin URI: https://frymo.de
- * Version: 1.17.1
+ * Version: 1.17.2
  * Description: Custom functions for Domu.
  * Author: Stark Systems UG
  * Author URI: https://stark-systems.de
